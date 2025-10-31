3bbc173da67f21cdcfe2b77e70b07c0e3e7186b7
francesccantallops <francesccantallops@alumnat.iesdamiahuguet.net>
2025-10-31 09:47:02 +0100

    Afegit sql Departaments

---
+++ SQL_Scripts/Departaments.sql
@@ -0,0 +1,7 @@
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ("Departament de Matemàtiques");
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ("Departament d'Educació Física");
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ('Departament de Física');
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ('Departament de Informàtica');
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ('Departament de Llengua Catalana i Literatura');
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ('Departamento de Lengua Castellana y Literatura');
+INSERT INTO `sortides_francesc`.`Departaments` (`Nom`) VALUES ('Department of English');
