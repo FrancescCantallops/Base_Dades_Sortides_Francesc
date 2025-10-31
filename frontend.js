c7bfd0e802676fe42f466bec290ea57b9d8e8251
francesccantallops <francesccantallops@alumnat.iesdamiahuguet.net>
2025-10-31 09:41:16 +0100

    Simplificacio i parametritzacio parcial de creador de blocs sortides

--- frontend.js
+++ frontend.js
@@ -1,13 +1,15 @@
 let isDeployed = [];
 console.log("Carregat array");
 
+let llocs = ["Serra de Tramuntana", "Fundació Miró", "Aqualand", "Aficine Palma", "Palma Jump"];
+let dates = ["12/10/2025", "15/10/2025", "3/11/2025", "5/3/2026", "6/5/2026"];
 
 function build_page(){
     let nombre_sortides = 5;
     for (let i=0; i<nombre_sortides; i++){
         document.getElementById("container").innerHTML += `
         <div class="bloc"> 
-            <button class="desplegador" onclick="toggle_deploy(${i})"></button> <div class="lloc">Lloc</div>   <div class="data">Data</div> 
+            <button class="desplegador" onclick="toggle_deploy(${i})"></button> <div class="lloc">${llocs[i]}</div>   <div class="data">${dates[i]}</div> 
             <div class="desplegable" id="desplegable_${i}"> 
                 <hr>
                 <div>Departament</div> <div>Hora sortida - Hora arribada</div>
@@ -22,10 +24,6 @@ function build_page(){
 
 
 function toggle_deploy(id){
-    console.log("Toggle activat");
-    console.log("Valor de isDeployed"+id+": "+isDeployed[id]);
     document.getElementById("desplegable_"+id).hidden = isDeployed[id];
     isDeployed[id] = !isDeployed[id];
-    console.log("Toggle acabat");
-    console.log("Valor de isDeployed"+id+": "+isDeployed[id]);
 }
