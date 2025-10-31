f5a9cb6824586a54f2f5be66841e124b0a2ba870
francesccantallops <francesccantallops@alumnat.iesdamiahuguet.net>
2025-10-31 17:48:46 +0100

    Funcio Construir Bloc

--- frontend.js
+++ frontend.js
@@ -1,50 +1,68 @@
 let isDeployed = [];
-console.log("Carregat array");
+console.log("Carregat array isDeployed");
 
 let llocs = ["Serra de Tramuntana", "Fundació Miró", "Aqualand", "Aficine Palma", "Palma Jump"];
 let dates = ["12/10/2025", "15/10/2025", "3/11/2025", "5/3/2026", "6/5/2026"];
 
 function build_sortides(){
-    document.getElementById("container").innerHTML = "";
     let nombre_sortides = 5;
+    document.getElementById("container").innerHTML = "";
+    isDeployed = [];
     for (let i=0; i<nombre_sortides; i++){
-        document.getElementById("container").innerHTML += `
-        <div class="bloc"> 
-            <button class="desplegador" onclick="toggle_deploy(${i})"><img class="img_boto" src="Imatges/down_arrow_simple_1.png"></button> <div class="lloc">${llocs[i]}</div>   <div class="data">${dates[i]}</div> 
-            <div class="desplegable" id="desplegable_${i}"> 
-                <hr>
-                <div>Departament</div> <div>Hora sortida - Hora arribada</div>
-                <div> Observacions jashjkdhjksahd </div>
-            </div>
-        </div>
-        `
-        isDeployed = [];
-        document.getElementById("desplegable_"+i).hidden = true;
-        isDeployed.push(false);
+        build_bloc(i);
+        document.getElementById("tagLeft"+i).innerHTML = llocs[i];
+        document.getElementById("tagRight"+i).innerHTML = dates[i];
+
+        document.getElementById("desplegable"+i) += `
+        <div> Departament </div>
+        <div> Hora sortida - Hora arribada </div>
+        <div> Observacions </div>
+        `;
     } 
 }
 
 function build_professors() {
-    document.getElementById("container").innerHTML = "";
     let nombre_professors = 7;
+    document.getElementById("container").innerHTML = "";
+    isDeployed = [];
     for (let i=0; i<nombre_professors; i++){
-        document.getElementById("container").innerHTML += `
-        <div class="bloc"> 
-            <button class="desplegador" onclick="toggle_deploy(${i})"><img class="img_boto" src="Imatges/down_arrow_simple_1.png"></button> <div class="lloc">Nom i Llinatges</div>   <div class="data">DNI</div>
-            <div class="desplegable" id="desplegable_${i}"> 
-                <hr>
-                <div>Sortides</div>
-            </div>
-        </div>
-        `
-        isDeployed = [];
-        document.getElementById("desplegable_"+i).hidden = true;
-        isDeployed.push(false);
+        build_bloc(i);
+        document.getElementById("tagLeft"+i).innerHTML = "Llinatges, Nom";
+        document.getElementById("tagRight"+i).innerHTML = "DNI:";
+
+        document.getElementById("desplegable"+i).innerHTML += "<div> sortides </div>";
     } 
 }
 
 
 function toggle_deploy(id){
-    document.getElementById("desplegable_"+id).hidden = isDeployed[id];
+    document.getElementById("desplegable"+id).hidden = isDeployed[id];
     isDeployed[id] = !isDeployed[id];
+    console.log("Tamany array isDeployed: "+isDeployed.length);
+}
+
+function build_bloc(i) {
+    document.getElementById("container").innerHTML += `
+        <div class="bloc"> 
+            <button class="desplegador" onclick="toggle_deploy(${i})">
+                <img class="img_boto" src="Imatges/down_arrow_simple_1.png">
+            </button> 
+            <div class="tag_left" id="tagLeft${i}"></div>
+            <div class="tag_right" id="tagRight${i}"></div>
+
+            <div class="desplegable" id="desplegable${i}"> 
+                <hr>
+            </div>
+        </div>
+        `;
+        document.getElementById("desplegable"+i).hidden = true;
+        isDeployed.push(false);
+}
+
+function build_blocs(n){
+    document.getElementById("container").innerHTML = "";
+    isDeployed = [];
+    for (let i=0; i<n; i++){
+        build_bloc(i);
+    }
 }
